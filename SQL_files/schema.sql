6910d986ee207a44f8b89756f77bf22ac9b1a473
AIPEAC <gptalnum@gmail.com>
2026-03-17 11:15:36 +0800

    style(sql): standardize table creation syntax

--- SQL_files/schema.sql
+++ SQL_files/schema.sql
@@ -1,130 +1,131 @@
-CREATE table Client
+CREATE TABLE Client
 (
-clientID INT IDENTITY(1,1) PRIMARY KEY,
-serviceTier varchar (255) not null,
-companyName varchar (255) not null,
-startDate date not null,
-contactPerson varchar (255)
+  clientID INT IDENTITY(1,1) PRIMARY KEY,
+  serviceTier varchar(255) NOT NULL,
+  companyName varchar(255) NOT NULL,
+  startDate date NOT NULL,
+  contactPerson varchar(255)
 );
 GO
 
-CREATE table Product
+CREATE TABLE Product
 (
-productID INT IDENTITY(1,1) PRIMARY KEY,
-name varchar (255) not null,
-brand varchar (255) not null,
-cost DECIMAL(38, 2) not null,
-category varchar (255) not null,
-price DECIMAL(38, 2) not null,
-length DECIMAL(38, 2) not null,
-width DECIMAL(38, 2) not null,
-height DECIMAL(38, 2) not null
+  productID INT IDENTITY(1,1) PRIMARY KEY,
+  name varchar(255) NOT NULL,
+  brand varchar(255) NOT NULL,
+  cost DECIMAL(38, 2) NOT NULL,
+  category varchar(255) NOT NULL,
+  price DECIMAL(38, 2) NOT NULL,
+  length DECIMAL(38, 2) NOT NULL,
+  width DECIMAL(38, 2) NOT NULL,
+  height DECIMAL(38, 2) NOT NULL
 );
 GO
 
-CREATE table ProductHandling
+CREATE TABLE ProductHandling
 (
-productID INT,
-handlingRequirement varchar(255) not null,
-FOREIGN KEY (productID) REFERENCES Product(productID),
-PRIMARY KEY (productID, handlingRequirement)
+  productID INT,
+  handlingRequirement varchar(255) NOT NULL,
+  FOREIGN KEY (productID) REFERENCES Product(productID),
+  PRIMARY KEY (productID, handlingRequirement)
 );
 GO
 
-CREATE table Supplier
+CREATE TABLE Supplier
 (
-supplierID INT IDENTITY(1,1) PRIMARY KEY,
-leadTime INT not null,
-paymentTerms varchar(255) not null,
-name varchar(255) not null,
-country varchar(255) not null
+  supplierID INT IDENTITY(1,1) PRIMARY KEY,
+  leadTime INT NOT NULL,
+  paymentTerms varchar(255) NOT NULL,
+  name varchar(255) NOT NULL,
+  country varchar(255) NOT NULL
 );
 GO
 
-CREATE table Supply
+CREATE TABLE Supply
 (
-period date not null,
-clientID INT not null,
-supplierID INT not null,
-productID INT not null,
-FOREIGN KEY (clientID) REFERENCES Client(clientID),
-FOREIGN KEY (supplierID) REFERENCES Supplier(supplierID),
-FOREIGN KEY (productID) REFERENCES Product(productID),
-PRIMARY KEY (period, clientID, supplierID, productID)
+  period date NOT NULL,
+  clientID INT NOT NULL,
+  supplierID INT NOT NULL,
+  productID INT NOT NULL,
+  FOREIGN KEY (clientID) REFERENCES Client(clientID),
+  FOREIGN KEY (supplierID) REFERENCES Supplier(supplierID),
+  FOREIGN KEY (productID) REFERENCES Product(productID),
+  PRIMARY KEY (period, clientID, supplierID, productID)
 );
 GO
 
-CREATE table Warehouse
+CREATE TABLE Warehouse
 (
-warehouseID INT IDENTITY(1,1) PRIMARY KEY,
-address varchar(255) not null,
-size DECIMAL(38, 2) not null,
-temperature DECIMAL(4, 1) not null,
-security varchar(255) not null,
+  warehouseID INT IDENTITY(1,1) PRIMARY KEY,
+  address varchar(255) NOT NULL,
+  size DECIMAL(38, 2) NOT NULL,
+  temperature DECIMAL(4, 1) NOT NULL,
+  security varchar(255) NOT NULL
 );
 GO
 
-CREATE table Zone
+CREATE TABLE Zone
 (
-warehouseID INT not null,
-location varchar (255) not null,
-code varchar (255) not null,
-FOREIGN KEY (warehouseID) REFERENCES Warehouse(warehouseID),
-PRIMARY KEY (warehouseID, location),
+  warehouseID INT NOT NULL,
+  location varchar(255) NOT NULL,
+  code varchar(255) NOT NULL,
+  FOREIGN KEY (warehouseID) REFERENCES Warehouse(warehouseID),
+  PRIMARY KEY (warehouseID, location)
 );
 GO
 
-CREATE table Inventory
+CREATE TABLE Inventory
 (
-warehouseID INT not null,
-productID INT not null,
-clientID INT not null,
-serial# INT not null,
-reservedQty INT not null,
-handQty INT not null,
-orderedQty INT not null,
-location varchar(255) not null,
-FOREIGN KEY (warehouseID) REFERENCES Warehouse(warehouseID),
-FOREIGN KEY (productID) REFERENCES Product(productID),
-FOREIGN KEY (clientID) REFERENCES Client(clientID),
-PRIMARY KEY (warehouseID, productID, clientID, serial#)
+  warehouseID INT NOT NULL,
+  productID INT NOT NULL,
+  clientID INT NOT NULL,
+  serial# INT NOT NULL,
+  reservedQty INT NOT NULL,
+  handQty INT NOT NULL,
+  orderedQty INT NOT NULL,
+  location varchar(255) NOT NULL,
+  FOREIGN KEY (warehouseID) REFERENCES Warehouse(warehouseID),
+  FOREIGN KEY (productID) REFERENCES Product(productID),
+  FOREIGN KEY (clientID) REFERENCES Client(clientID),
+  PRIMARY KEY (warehouseID, productID, clientID, serial#)
 );
 GO
 
 
 
-CREATE table InventoryMovement
+CREATE TABLE InventoryMovement
 (
-warehouseID INT not null,
-productID INT not null,
-clientID INT not null,
-serial# INT not null,
-movement varchar(255) not null,
-reason varchar(255) not null,
-timestamp timestamp not null,
-FOREIGN KEY (warehouseID) REFERENCES Warehouse(warehouseID),
-FOREIGN KEY (productID) REFERENCES Product(productID),
-FOREIGN KEY (clientID) REFERENCES Client(clientID),
-FOREIGN KEY (warehouseID, productID, clientID, serial#)
-        REFERENCES Inventory(warehouseID, productID, clientID, serial#),
-PRIMARY KEY (warehouseID, productID, clientID, serial#, movement, reason, timestamp)
+  warehouseID INT NOT NULL,
+  productID INT NOT NULL,
+  clientID INT NOT NULL,
+  serial# INT NOT NULL,
+  movement varchar(255) NOT NULL,
+  reason varchar(255) NOT NULL,
+  timestamp timestamp NOT NULL,
+  FOREIGN KEY (warehouseID) REFERENCES Warehouse(warehouseID),
+  FOREIGN KEY (productID) REFERENCES Product(productID),
+  FOREIGN KEY (clientID) REFERENCES Client(clientID),
+  FOREIGN KEY (warehouseID, productID, clientID, serial#)
+    REFERENCES Inventory(warehouseID, productID, clientID, serial#),
+  PRIMARY KEY (warehouseID, productID, clientID, serial#, movement, reason, timestamp)
 );
 GO
 
-CREATE table Item
+CREATE TABLE Item
 (
-itemSerial# INT IDENTITY(1,1) PRIMARY KEY,
-productID INT not null,
-FOREIGN KEY (productID) REFERENCES Product(productID)
+  itemSerial# INT IDENTITY(1,1) PRIMARY KEY,
+  productID INT NOT NULL,
+  FOREIGN KEY (productID) REFERENCES Product(productID)
 );
+GO
 
 --OrderItem
 
-CREATE table PurchaseOrder
+CREATE TABLE PurchaseOrder
 (
-orderID INT IDENTITY(1,1) PRIMARY KEY,
-orderDate date not null,
-status varchar (255) not null,
+  orderID INT IDENTITY(1,1) PRIMARY KEY,
+  orderDate date NOT NULL,
+  status varchar(255) NOT NULL
 );
 GO
 
@@ -132,36 +133,36 @@ GO
 --PurchaseOrder_Supplier
 --ShipItem
 
-CREATE table Shipment
+CREATE TABLE Shipment
 (
-shipmentID INT IDENTITY(1,1) PRIMARY KEY,
-exArrDate date not null,
-acArrDate date not null,
-shippedDate date not null,
-originalLocation varchar(255) not null,
-trackingNumber INT not null,
-orderID INT not null,
-FOREIGN KEY (orderID) REFERENCES PurchaseOrder(orderID)
+  shipmentID INT IDENTITY(1,1) PRIMARY KEY,
+  exArrDate date NOT NULL,
+  acArrDate date NOT NULL,
+  shippedDate date NOT NULL,
+  originalLocation varchar(255) NOT NULL,
+  trackingNumber INT NOT NULL,
+  orderID INT NOT NULL,
+  FOREIGN KEY (orderID) REFERENCES PurchaseOrder(orderID)
 );
 GO
 
 --Shipment_Supplier
 --Shipment_Warehouse
 
-CREATE table Staff
+CREATE TABLE Staff
 (
- staffID INT IDENTITY(1,1) PRIMARY KEY,
- name varchar(255) not null,
- type varchar(255) not null,
- hireDate date not null
- );
+  staffID INT IDENTITY(1,1) PRIMARY KEY,
+  name varchar(255) NOT NULL,
+  type varchar(255) NOT NULL,
+  hireDate date NOT NULL
+);
 GO
 
-CREATE table Employee
+CREATE TABLE Employee
 (
-  staffID INT not null,
-  certification VARCHAR(255) not null,
-  warehouseID INT not null, 
+  staffID INT NOT NULL,
+  certification varchar(255) NOT NULL,
+  warehouseID INT NOT NULL,
   FOREIGN KEY (staffID) REFERENCES Staff(staffID),
   FOREIGN KEY (warehouseID) REFERENCES Warehouse(warehouseID)
 );
