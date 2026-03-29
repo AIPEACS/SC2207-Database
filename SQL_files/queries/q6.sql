a8a8e9ab4c5487290cd280b095070dadd1ea0eb6
AIPEAC <203755544+AIPEAC@users.noreply.github.com>
2026-03-29 12:20:33 +0800

    remove commented

--- SQL_files/queries/q6.sql
+++ SQL_files/queries/q6.sql
@@ -1,34 +1,3 @@
--- (
--- 	SELECT Supplier.supplierID AS supplierID, Supplier.name AS name
--- 	FROM Supplier
--- 	WHERE NOT EXISTS (
--- 		SELECT 1
--- 		FROM Supplier sr
--- 		JOIN Shipment_Supplier shsr ON sr.supplierID = shsr.supplierID
--- 		JOIN Shipment_Warehouse shw ON shsr.shipmentID = shw.shipmentID
--- 		JOIN Warehouse w ON w.warehouseID = shw.warehouseID
--- 		WHERE w.address = 'Thailand'
--- 			AND Supplier.supplierID = sr.supplierID
--- 	)
--- ) INTERSECT (
--- 	SELECT Supplier.supplierID AS supplierID, Supplier.name AS name
--- 	FROM Supplier
--- 	WHERE NOT EXISTS (
--- 		SELECT w.warehouseID
--- 		FROM Warehouse w
--- 		WHERE w.address = 'Singapore'
--- 			AND NOT EXISTS (
--- 				SELECT sr.supplierID
--- 				FROM Supplier sr
--- 				JOIN Shipment_Supplier shsr ON sr.supplierID = shsr.supplierID
--- 				JOIN Shipment_Warehouse shw ON shsr.shipmentID = shw.shipmentID
--- 				JOIN Warehouse w1 ON w1.warehouseID = shw.warehouseID
--- 				WHERE Supplier.supplierID = sr.supplierID
--- 					AND w1.warehouseID = w.warehouseID
--- 			)
--- 	)
--- )
-
 WITH SingaporeProduct(productID, clientID) AS (
     SELECT DISTINCT i.productID, i.clientID
     FROM Inventory i
